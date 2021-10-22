e0330ec90a4577476930cf6225be441f6933af6e
Eman Mahmoud <60985699+EmanMahm0ud@users.noreply.github.com>
2021-10-22 23:01:26 +0200

    build nav bar dynamically

--- js/app.js
+++ js/app.js
@@ -23,14 +23,6 @@
  * 
 */
 
-let section1 = document.getElementById('section1');
-let section2 = document.getElementById('section2');
-let section3 = document.getElementById('section3');
-
-let navSection1 = document.getElementById('nav-section1');
-let navSection2 = document.getElementById('nav-section2');
-let navSection3 = document.getElementById('nav-section3');
-
 /**
  * End Global Variables
  * Start Helper Functions
@@ -68,6 +60,28 @@ let navSection3 = document.getElementById('nav-section3');
 
 /* -------------------------------- */
 
+// building nav bar dynamically  
+let ul = document.getElementById('navbar__list');
+for (let i = 0; i < 4; i++) {
+    let newList = document.createElement('li');
+    newList.id = 'nav-section' + String(i+1);
+    newList.innerText = 'Section ' + String(i+1);
+    
+    ul.appendChild(newList);
+}
+
+
+// declaring variables
+let section1 = document.getElementById('section1');
+let section2 = document.getElementById('section2');
+let section3 = document.getElementById('section3');
+let section4 = document.getElementById('section4');
+
+let navSection1 = document.getElementById('nav-section1');
+let navSection2 = document.getElementById('nav-section2');
+let navSection3 = document.getElementById('nav-section3');
+let navSection4 = document.getElementById('nav-section4');
+
 // open/close the nav bar list when you press on the toggle button
 function openMenu() {
     let ulState = document.getElementById("navbar__list");
@@ -87,27 +101,47 @@ document.addEventListener('click', function(e) {
         section2.scrollIntoView({behavior: "smooth"});
     } else if (e.target.id == "nav-section3") {
         section3.scrollIntoView({behavior: "smooth"});
+    } else if (e.target.id == "nav-section4") {
+        section4.scrollIntoView({behavior: "smooth"});
     }
 });
 
 // avtivating the section in the nav bar while scrolling
 document.addEventListener('scroll', function() {
     // subtracting from 40 (the heigth of the nav bar)
-    if (window.scrollY >= section1.offsetTop - 40 && window.scrollY < section2.offsetTop - 40) {
-        navSection1.className = 'active';
-        navSection2.className = '';
-        navSection3.className = '';
-    } else if (window.scrollY >= section2.offsetTop - 40 && window.scrollY < section3.offsetTop - 40) {
-        navSection1.className = '';
-        navSection2.className = 'active';
-        navSection3.className = '';
-    } else if (window.scrollY >= section3.offsetTop - 40 && window.scrollY < section3.offsetTop + section3.offsetHeight - 40) {
-        navSection1.className = '';
-        navSection2.className = '';
-        navSection3.className = 'active';
+    if (section1.getBoundingClientRect().top < 40 && section1.getBoundingClientRect().bottom > 40) {
+        navSection1.classList.add('active');
+        navSection2.classList.remove('active');
+
+        section1.classList.add('active');
+        section2.classList.remove('active');
+    } else if (section2.getBoundingClientRect().top < 40 && section2.getBoundingClientRect().bottom > 40) {
+        navSection2.classList.add('active');
+        navSection1.classList.remove('active');
+        navSection3.classList.remove('active');
+
+        section2.classList.add('active');
+        section1.classList.remove('active');
+        section3.classList.remove('active');
+    } else if (section3.getBoundingClientRect().top < 40 && section3.getBoundingClientRect().bottom > 40) {
+        navSection3.classList.add('active');
+        navSection2.classList.remove('active');
+        navSection4.classList.remove('active');
+
+        section3.classList.add('active');
+        section2.classList.remove('active');
+        section4.classList.remove('active');
+    } else if (section4.getBoundingClientRect().top < 40 && section4.getBoundingClientRect().bottom > 40) {
+        navSection4.classList.add('active');
+        navSection3.classList.remove('active');
+
+        section4.classList.add('active');
+        section3.classList.remove('active');
     } else {
-        navSection1.className = '';
-        navSection2.className = '';
-        navSection3.className = '';
+        navSection1.classList.remove('active');
+        navSection4.classList.remove('active');
+
+        section1.classList.remove('active');
+        section4.classList.remove('active');
     }
 });
